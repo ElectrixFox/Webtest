7dd0e12ad59d46b93582132d84b56c8a0c2b9198
MathematicalMoose <jazzy.tophat.man@gmail.com>
2022-07-04 18:12:44 +0100

    New commit

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
 console.log("Hello");
-function hoaruhfvaiu(){
 
-    Image im = 
+function nim()
+{
+    var im = new Image();
+    im.src = 'Images/Sharp.png';
+    im.id = 'nt';
+    document.getElementById('notes').appendChild(im); 
+}
+
+function position(nme, x, y)
+{
+    var e = document.getElementById(nme);
+
+    e.position.x = x;
+    e.position.y = y;
+
 }
